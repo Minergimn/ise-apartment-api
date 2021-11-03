ea7e1c69c6f57493417127d86454cdc404e4fbe5
Victor Deryabin <minergimn@gmail.com>
2021-11-03 14:51:40 +0300

    task-3: more validations, api dumb implementation with check, log and nil retuning

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,16 +3,14 @@ package api
 import (
 	"context"
 
+	"github.com/ozonmp/ise-apartment-api/internal/repo"
+	pb "github.com/ozonmp/ise-apartment-api/pkg/ise-apartment-api"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	"github.com/ozonmp/ise-apartment-api/internal/repo"
-
-	pb "github.com/ozonmp/ise-apartment-api/pkg/ise-apartment-api"
 )
 
 var (
@@ -66,4 +64,55 @@ func (o *apartmentAPI) DescribeApartmentV1(
 			Owner: apartment.Owner,
 		},
 	}, nil
+}
+
+func (o *apartmentAPI) CreateApartmentV1(
+	ctx context.Context,
+	req *pb.CreateApartmentV1Request,
+) (*pb.CreateApartmentV1Response, error){
+
+	if err := req.Validate(); err != nil {
+		log.Error().Err(err).Msg("CreateApartmentV1 - invalid argument")
+
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	//o.repo.CreateApartment calling will be here
+	log.Debug().Str("CreateApartmentV1 input", req.String()).Msg("Just a log instead of an implementation")
+
+	return nil, nil
+}
+
+func (o *apartmentAPI) ListApartmentsV1(
+	ctx context.Context,
+	req *pb.ListApartmentsV1Request,
+) (*pb.ListApartmentsV1Response, error){
+
+	if err := req.Validate(); err != nil {
+		log.Error().Err(err).Msg("ListApartmentsV1 - invalid argument")
+
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	//o.repo.ListApartments calling will be here
+	log.Debug().Str("ListApartmentsV1 input", req.String()).Msg("Just a log instead of an implementation")
+
+	return nil, nil
+}
+
+func (o *apartmentAPI) RemoveApartmentV1(
+	ctx context.Context,
+	req *pb.RemoveApartmentV1Request,
+) (*pb.RemoveApartmentV1Response, error){
+
+	if err := req.Validate(); err != nil {
+		log.Error().Err(err).Msg("RemoveApartmentV1 - invalid argument")
+
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	//o.repo.DeleteApartment calling will be here
+	log.Debug().Str("RemoveApartmentV1 input", req.String()).Msg("Just a log instead of an implementation")
+
+	return nil, nil
 }
